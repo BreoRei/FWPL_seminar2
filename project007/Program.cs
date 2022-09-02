b67e7cdb13a478287830e587fd2c921cc81b55b6
smilezz <lihachev647@gmail.com>
2022-09-02 13:01:35 +0300

    some correct in pr007

--- project007/Program.cs
+++ project007/Program.cs
@@ -16,7 +16,7 @@ if (x1 > x2) {
     x1 = x2;
     x2 = help;
 }
-if (x3 > x4){
+if (x3 > x4) {
     help = x3;
     x3 = x4;
     x4 = help;
@@ -33,4 +33,3 @@ if (x1 < x3 && x2 > x3) {
 } else {
     Console.WriteLine("Не пересекаются");
 }
-
